5d214f9bcf6d0cd5123b38dc88fe7e8d20a3dc84
asand017 <asand017@ucr.edu>
2020-01-30 14:28:05 -0800

    resize cloud output

--- movie_cloud/urls.py
+++ movie_cloud/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
 	path('wordCloud/', include('wordCloud.urls', namespace="app_name")),
 ]
 
-if settings.DEBUG:
+#if settings.DEBUG:
 	#urlpatterns += staticfiles_urlpatterns()
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
